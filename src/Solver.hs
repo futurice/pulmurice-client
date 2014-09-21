f3d03e1645a17eb480f76d50497c7338c1c0bc86
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-09-21 16:32:50 +0300

    Add square solution as an example of reading non-string inputs

--- src/Solver.hs
+++ src/Solver.hs
@@ -1,8 +1,14 @@
 module Solver where
 
+import Text.Read
+
 solveEcho :: String -> String
 solveEcho = id
 
+solveSquare :: Integer -> String
+solveSquare x = show (x * x)
+
 solve :: String -> String -> Maybe String
-solve "echo" input = Just (solveEcho input)
-solve _ _          = Nothing
+solve "echo"   input = Just (solveEcho input)
+solve "square" input = solveSquare `fmap` readMaybe input
+solve _ _            = Nothing
